1b22599d6bd2eb314594524a05a898d5f308da63
브니 <161662792+DaramgeOvO@users.noreply.github.com>
2024-07-25 11:06:15 +0900

    commit

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -1,10 +1,27 @@
-import { Gallery } from "./Components/20240723/Gallery";
+import { createGlobalStyle } from "styled-components";
+import { NinjasAPI } from "./Components/OpenWeatherMap/NinjasAPI";
+import { OpenWeather } from "./Components/OpenWeatherMap/OpenWeather";
 
+const GlobalStyle = createGlobalStyle`
+@font-face {
+    font-family: 'GmarketSansMedium';
+    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: GmarketSansMedium;
+  }
+`;
 
 function App() {
   return (
     <>
-      <Gallery />
+      <GlobalStyle />
+      <OpenWeather />
     </>
   );
 }
